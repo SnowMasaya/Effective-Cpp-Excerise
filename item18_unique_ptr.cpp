f5a79dada66977804db324b3c37faa6e52887bb6
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-01-11 13:01:11 +0900

    update unique ptr

---
+++ item18_unique_ptr.cpp
@@ -0,0 +1,41 @@
+
+/*
+ * case1_const.cpp
+ *
+ *  Created on: Jan 8, 2017
+ *      Author: masayaogushi
+ */
+#include<iostream>
+#include<memory>
+using namespace std;
+
+struct Foo
+{
+	Foo() {cout << "Foo:Foo" << endl;}
+	~Foo() {cout << "Foo:~Foo" << endl;}
+	void bar() {cout << "Foo:bar" << endl;}
+};
+
+void f(const Foo &)
+{
+	cout << "f(const Foo&)" << endl;
+}
+
+int tmp_main19()
+{
+	unique_ptr<Foo> p1(new Foo);
+
+	if(p1) p1->bar();
+
+	{
+		unique_ptr<Foo> p2(move(p1));
+		f(*p2);
+
+		p1 = move(p2);
+		cout << "destorying p2..." << endl;
+	}
+
+	if(p1) p1->bar();
+
+	return 0;
+}
