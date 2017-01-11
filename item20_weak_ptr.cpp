828af05a8fddac2296feaf978612a6422c35c3fa
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-01-11 16:17:05 +0900

    weak pointer

---
+++ item20_weak_ptr.cpp
@@ -0,0 +1,37 @@
+
+/*
+ * case1_const.cpp
+ *
+ *  Created on: Jan 8, 2017
+ *      Author: masayaogushi
+ */
+
+#include<iostream>
+#include<memory>
+
+using namespace std;
+
+weak_ptr<int> gw;
+
+void f()
+{
+	cout << "use count == " << gw.use_count() << ": ";
+	if(auto spt = gw.lock()){
+		cout << *spt << endl;
+	}
+	else {
+		cout << "gw is expired" << endl;
+	}
+}
+
+int main()
+{
+	{
+		auto sp = make_shared<int>(42);
+		gw = sp;
+
+		f();
+	}
+
+	f();
+}
