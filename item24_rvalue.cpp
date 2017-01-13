8dfb836f07700604872c056bed37100f5717cb18
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-01-13 11:10:27 +0900

    rvalue

---
+++ item24_rvalue.cpp
@@ -0,0 +1,39 @@
+
+/*
+ * case1_const.cpp
+ *
+ *  Created on: Jan 8, 2017
+ *      Author: masayaogushi
+ */
+
+#include<iostream>
+#include<utility>
+
+using namespace std;
+
+void f(int& x)
+{
+	cout << "lvalue reference overload f(" << x << ")" << endl;
+}
+
+void f(const int& x)
+{
+	cout << "lvalue referenceto const overload f(" << x << ")" << endl;
+}
+
+void f(int&& x)
+{
+	cout << "rvalue referenceto const overload f(" << x << ")" << endl;
+}
+
+int main()
+{
+	int i = 1;
+	const int ci = 2;
+
+	f(i);
+	f(ci);
+	f(3);
+
+	f(std::move(i));
+}
